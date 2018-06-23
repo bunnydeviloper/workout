018f97d5a891566bfd187899e100a0e0adbdb7a9
Sophia <phuongmai001@gmail.com>
2018-06-23 14:06:58 -0700

    update counter, quote, and modal

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
-const q1 = "GOOD THINGS COME TO THOSE WHO SWEAT";
-const q2 = "HUSTLE FOR THAT MUSCLE";
-const q3 = "PUSH HARDER THAN YESTERDAY IF YOU WANT A DIFFERENT TOMORROW";
-const q5 = "RESULTS HAPPEN OVERTIME, NOT OVERNIGHT. BE PATIENT";
-const q6 = "IT COMES DOWN TO ONE SIMPLE THING: HOW BAD DO YOU WANT IT?";
-const q7 = "WORKOUT IS HARD, BEING FAT IS HARD, PICK YOUR HARD";
-const q8 = "IF IT DOESN'T CHALENGE YOU, IT DOESN'T CHANGE YOU";
-const q9 = "NOT EVERY IS A GOOD DAY. SHOW UP ANYWAY!";
+window.onload = function() { startWorkout() };
 
-const quotes = [q1, q2, q3, q4, q5, q6, q7, q8, q9];
-
-// create component for "text", this will display number of reps, count +1 each click
 // create component to display duration, has a start and stop one reps = 1000
-// randomize quotes and display on the page
 // pick 10 hot pictures and display each time rep = 100
-//
-//
-// number
-const number = document.createElement('h1');
-let counter = 0;
-number.innerHTML = counter;
 
+let counter = 0;
 let keys;
 
-window.addEventListener('keydown', function(e) {
-  keys = keys || [];
-  keys[e.keyCode] = (e.type == "keydown"); // will be true if key is being pressed
-});
-window.addEventListener('keyup', function(e) {
-  keys[e.keyCode] = (e.type == "keydown"); // will be false if key is not pressed
-});
+const newh2 = document.createElement('h2');
 
+function startWorkout() {
+  counter = 0;
+  document.body.appendChild(newh2);
+  newh2.innerHTML = counter;
 
-if (keys && keys.length > 0) {
-  if (keys[83]) counter++;
+  update();
 }
-keys = []; // soft reset?
 
+function update() {
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) {
+      updateCounter();
+    }
+  }
+}
 
-// quote
+function updateCounter() {
+  counter++;
+  newh2.innerHTML = counter;
+
+  // display a new quote everytime user hit 50 count
+  if (counter === 1 || counter % 5 === 0) {
+    displayQuotes();
+  }
+
+  // display a congratulations modal when user finished 1000 count
+  if (counter === 1000) {
+    displayCongrats();
+  }
+}
+
+function displayQuotes() {
+  const q0 = "COME ON, YOU CAN DO IT!!!";
+  const q1 = "GOOD THINGS COME TO THOSE WHO SWEAT";
+  const q2 = "HUSTLE FOR THAT MUSCLE";
+  const q4 = "PUSH HARDER THAN YESTERDAY IF YOU WANT A DIFFERENT TOMORROW";
+  const q3 = "IF YOU START NOW YOU'LL START SEEING RESULTS ONE DAY EARLIER!!!";
+  const q5 = "RESULTS HAPPEN OVERTIME, NOT OVERNIGHT. BE PATIENT";
+  const q6 = "IT COMES DOWN TO ONE SIMPLE THING: HOW BAD DO YOU WANT IT?";
+  const q7 = "WORKOUT IS HARD, BEING FAT IS HARD, PICK YOUR HARD";
+  const q8 = "IF IT DOESN'T CHALENGE YOU, IT DOESN'T CHANGE YOU";
+  const q9 = "NOT EVERY IS A GOOD DAY. SHOW UP ANYWAY!";
+
+  const quotes = [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9];
+
+  const motivation = document.getElementById('motivation');
+  const random = function(max, min) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  motivation.style.color = "red";
+  motivation.innerHTML = quotes[random(0, 9)];
+}
+
+function displayCongrats() {
+  const congrats = document.getElementById('congrats');
+  congrats.style.display = "flex";
+}
+
 // duration
 // picture modal
 // congrats modal
